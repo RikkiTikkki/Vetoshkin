94cf9c21f210d6a2c6cba0061ea91782910eeaf8
RikkiTikkki <novet2020@gmail.com>
2026-04-01 23:19:40 +0500

    Add files via upload

---
+++ Лабораторная работа №8 (Ветошкин Никита) C#/Исходный код клиента.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛБ8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[10];
+            using (var s = new NamedPipeClientStream("Pipe_lab8"))
+            {
+                Console.WriteLine("Начинает работать клиент");
+                byte b;
+                s.Connect();
+                for (int i = 0; i < 10; i++)
+                    a[i] = (byte)(s.ReadByte());
+                Console.WriteLine("Полученный массив байтов:");
+                for (int i = 0; i < 10; i++)
+                    Console.Write(a[i] + "\t");
+                Console.WriteLine();
+                for (int i = 0; i < 9; i++)
+                    for (int j = i + 1; j < 10; j++)
+                        if (a[i] < a[j])
+                        { b = a[i]; a[i] = a[j]; a[j] = b; }
+                Console.WriteLine("Массив после сортировки:");
+                for (int i = 0; i < 10; i++)
+                    Console.Write(a[i] + "\t");
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                    s.WriteByte(a[i]);
+            }
+            Console.WriteLine("Сеанс клиента закончен");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Лабораторная работа №8 (Ветошкин Никита) C#/Исходный код сервера.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛБ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[10];
+            using (var s = new NamedPipeServerStream("Pipe_lab8"))
+            {
+                Random rnd = new Random();
+                Console.WriteLine("Массив создан: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    a[i] = (byte)(rnd.Next() % 101);
+                    Console.Write(a[i] + "\t");
+                }
+                Console.WriteLine();
+                s.WaitForConnection();
+                for (int i = 0; i < 10; i++)
+                    s.WriteByte(a[i]);
+                for (int i = 0; i < 10; i++)
+                    a[i] = (byte)(s.ReadByte());
+                Console.WriteLine("Массив после сортировки: ");
+                for (int i = 0; i < 10; i++)
+                    Console.Write(a[i] + "\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Сеанс сервера закончен");
+            Console.ReadLine();
+        }
+    }
+}
